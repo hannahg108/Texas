d72c0943a0d1ebe25e02622d4f032fd63682afdd
hannahg108 <85882197+hannahg108@users.noreply.github.com>
2021-06-29 08:26:36 -0500

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { LandmarksComponent } from './Landmarks/about.component';
+import { LandmarksComponent } from './Landmarks/Landmarks.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: LandmarksComponent },
+  { path: 'Landmarks', component: LandmarksComponent },
   { path: 'contact', component: ContactComponent },
 
   // otherwise redirect to home
