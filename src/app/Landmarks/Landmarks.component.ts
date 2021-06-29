3548f9ad6139e4cdc33ef33732d57eb01bb644ad
hannahg108 <85882197+hannahg108@users.noreply.github.com>
2021-06-29 08:04:45 -0500

    Landmarks

---
+++ src/app/Landmarks/Landmarks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Landmarks',
+  templateUrl: './Landmarks.component.html',
+  styleUrls: ['./Landmarks.component.scss']
+})
+export class LandmarksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TexasComponent } from './Texas/Texas.component';
-import { AboutComponent } from './about/about.component';
+import { LandmarksComponent } from './Landmarks/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
@@ -14,7 +14,7 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     TexasComponent,
-    AboutComponent,
+    LandmarksComponent,
     ContactComponent,
     HomeComponent
   ],
