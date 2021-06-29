b3d59515a3946610a165e7a53aa714b42b63fb23
hannahg108 <85882197+hannahg108@users.noreply.github.com>
2021-06-29 08:54:32 -0500

    Fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { LandmarksComponent } from './Landmarks/about.component';
+import { LandmarksComponent } from './Landmarks/Landmarks.component';
 import { ContactComponent } from './contact/contact.component';
 import { AdventuresComponent } from './adventures/adventures.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: LandmarksComponent },
+  { path: 'Landmarks', component: LandmarksComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'adventures', component: AdventuresComponent },
   // otherwise redirect to home
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TexasComponent } from './Texas/Texas.component';
-import { LandmarksComponent } from './Landmarks/about.component';
+import { LandmarksComponent } from './Landmarks/Landmarks.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { AdventuresComponent } from './adventures/adventures.component';
